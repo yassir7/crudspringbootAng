881a84562990f98dfb0114db3b5281d23d5de796
spaz <spaz@playtechla.com>
2021-10-20 15:26:48 -0500

    Capa de extraccion de datos. Implementa funciones para consultar por campo nombre

---
+++ api/src/main/java/com/pruebatecnica/api/repository/UsuarioRepository.java
@@ -0,0 +1,19 @@
+package com.pruebatecnica.api.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.pruebatecnica.api.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
+
+	@Query(value = "SELECT * FROM usuario WHERE nombre LIKE LOWER('%?1%') ORDER BY LOCATE('?1', nombre) ASC, nombre ASC", nativeQuery = true)
+	public List<Usuario> findLikeNombre(String nombre);
+	
+	public List<Usuario> findByNombre(String nombre);
+	
+	public List<Usuario> findAllByOrderByNombreAsc();
+}
