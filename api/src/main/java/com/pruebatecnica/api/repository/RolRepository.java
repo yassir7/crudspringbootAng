4ad178dd505224c7c81c4411c22616b3f71345ee
spaz <spaz@playtechla.com>
2021-10-20 15:27:14 -0500

    Capa de extraccion de datos de la tabla Rol

---
+++ api/src/main/java/com/pruebatecnica/api/repository/RolRepository.java
@@ -0,0 +1,10 @@
+package com.pruebatecnica.api.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pruebatecnica.api.model.Rol;
+
+public interface RolRepository extends JpaRepository<Rol, Integer>{
+
+}
