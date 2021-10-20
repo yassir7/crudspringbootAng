fc0905537edbef97b44106a068a50e1f0afab669
spaz <spaz@playtechla.com>
2021-10-20 15:25:35 -0500

    Modelo que representa la tabla rol de la base de datos

---
+++ api/src/main/java/com/pruebatecnica/api/model/Rol.java
@@ -0,0 +1,37 @@
+package com.pruebatecnica.api.model;
+
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Rol {
+
+	@Id
+	@Column(name = "id_rol", unique = true, nullable = false)
+	private Integer id;
+	private String nombre;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
