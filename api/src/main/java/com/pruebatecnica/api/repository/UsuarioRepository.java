25710c05c09a90ee2f317b60044406381b27e49c
spaz <spaz@playtechla.com>
2021-10-24 02:21:02 -0500

    Se elimina import innecesario.

--- api/src/main/java/com/pruebatecnica/api/repository/UsuarioRepository.java
+++ api/src/main/java/com/pruebatecnica/api/repository/UsuarioRepository.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
 import com.pruebatecnica.api.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
