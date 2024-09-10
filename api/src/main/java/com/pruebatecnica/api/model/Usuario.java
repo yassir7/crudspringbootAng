ad409b60e930d2f759e0d51ca025e8c58c5ef940
saadoun20000 <wsaad2901@gmail.com>
2024-09-10 14:23:17 +0100

    Update Usuario.java change dans la classe User

--- api/src/main/java/com/pruebatecnica/api/model/Usuario.java
+++ api/src/main/java/com/pruebatecnica/api/model/Usuario.java
@@ -10,7 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 @Entity
-public class User {
+public class usuarIO {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
