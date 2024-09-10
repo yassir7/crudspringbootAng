307f21fd33c5327d10a16cc84a805f29394333f7
saadoun20000 <wsaad2901@gmail.com>
2024-09-10 11:55:37 +0100

    Update Usuario.java le Nom de "User"

--- api/src/main/java/com/pruebatecnica/api/model/Usuario.java
+++ api/src/main/java/com/pruebatecnica/api/model/Usuario.java
@@ -10,7 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 @Entity
-public class Usuario {
+public class User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
