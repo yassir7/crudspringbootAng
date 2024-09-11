df9698654598563eb6fdab25b7083e076906a09b
saadoun20000 <wsaad2901@gmail.com>
2024-09-11 15:21:37 +0100

    Update Usuario.java  change to Person

--- api/src/main/java/com/pruebatecnica/api/model/Usuario.java
+++ api/src/main/java/com/pruebatecnica/api/model/Usuario.java
@@ -10,7 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 @Entity
-public class UserPartido {
+public class Person {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
